09a59e63d0e0a8761571802d108e44ac35037dbc
athumaniMfaume <athumanimfaume1995@gmail.com>
2026-02-09 15:33:32 +0300

    add readme 1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(), // Merged here
   ],
-  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
